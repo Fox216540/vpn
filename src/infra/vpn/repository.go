8c362e6da0b8c9c4c5031d76a4e3a26edfe48cb3
Andrey <snick25rs@gmail.com>
2025-09-19 00:40:47 +0200

    Добавил секундную задержку для предотвращения race condition при создании репозитория.

--- src/infra/vpn/repository.go
+++ src/infra/vpn/repository.go
@@ -42,6 +42,7 @@ func (r *Repository) Create(configID uuid.UUID) (string, error) {
 		_, _ = io.Copy(os.Stdout, ptmx)
 	}()
 
+	time.Sleep(1 * time.Second)
 	io.WriteString(ptmx, "1\n")
 	io.WriteString(ptmx, configID.String()+"\n")
 
