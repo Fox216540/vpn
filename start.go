6ffae9f2e4c1bfe51da7b3abff7d0a84a6b368c8
Andrey <snick25rs@gmail.com>
2025-09-19 00:35:04 +0200

    Добавил импорт пакета uuid, закомментировал вызов CreateServer и раскомментировал вызов Create с использованием uuid, а также раскомментировал вывод переменной fff.

--- start.go
+++ start.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/uuid"
 	"vpn/src/infra/vpn"
 )
 
@@ -13,12 +14,12 @@ func main() {
 	//if err != nil {
 	//	fmt.Println(err)
 	//}
-	err := vpn.NewRepository().CreateServer()
-	//fff, err := vpn.NewRepository().Create(uuid.New())
+	//err := vpn.NewRepository().CreateServer()
+	fff, err := vpn.NewRepository().Create(uuid.New())
 	//err := searchClient("foo3")
 	//lines, err := readAndFilterFile(filePath, "foo2")
 	if err != nil {
 		fmt.Println(err)
 	}
-	//fmt.Println(fff)
+	fmt.Println(fff)
 }
