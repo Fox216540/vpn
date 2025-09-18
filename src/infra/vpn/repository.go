eee82ea4df594607c574172a17999876ed4b9395
Andrey <snick25rs@gmail.com>
2025-09-19 00:50:32 +0200

    Удалил код, копировавший вывод PTY в stdout, так как он вызывал проблемы.

--- src/infra/vpn/repository.go
+++ src/infra/vpn/repository.go
@@ -38,10 +38,6 @@ func (r *Repository) Create(configID uuid.UUID) (string, error) {
 
 	defer func() { _ = ptmx.Close() }() // закрываем PTY после завершения
 
-	go func() {
-		_, _ = io.Copy(os.Stdout, ptmx)
-	}()
-
 	time.Sleep(20 * time.Millisecond)
 	io.WriteString(ptmx, "1\n")
 	io.WriteString(ptmx, configID.String()+"\n")
