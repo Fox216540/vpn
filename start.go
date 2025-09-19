0d9118d6e850337ab2d65c40c5a538e09b5c1ef0
Andrey <snick25rs@gmail.com>
2025-09-20 00:27:38 +0200

    Импортировал необходимые пакеты, настроил gRPC-сервер для сервиса конфигурации с перехватчиком аутентификации и зарегистрировал его.

--- start.go
+++ start.go
@@ -1,25 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"github.com/google/uuid"
-	"vpn/src/infra/vpn"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"vpn/src/api/config"
+	pb "vpn/src/api/config/proto"
+	"vpn/src/core/interceptor"
+	"vpn/src/infra/hasher"
 )
 
 func main() {
-	//startServer()
-	//startClient("foo6")
-	//time.Sleep(3 * time.Second)
-	uuidStr, err := uuid.Parse("6efc0c5c-4b42-4119-8979-463df786a5f6")
+	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("failed to listen: %v", err)
 	}
-	err = vpn.NewRepository().Delete(uuidStr)
-	//fff, err := vpn.NewRepository().Create(uuid.New())
-	//err := searchClient("foo3")
-	//lines, err := readAndFilterFile(filePath, "foo2")
-	if err != nil {
-		fmt.Println(err)
+
+	handler := config.NewHandler()
+	h := hasher.NewHasher()
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(interceptor.AuthUnaryInterceptor(h)),
+	)
+	pb.RegisterConfigServiceServer(server, handler)
+
+	log.Println("gRPC server is running on :50051")
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
-	//fmt.Println(fff)
+
 }
