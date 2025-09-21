7bead6c5b174eee6c0226f52d91f94515cb5e45d
Andrey <snick25rs@gmail.com>
2025-09-21 19:10:46 +0200

    Добавил переменные для интервалов сбора данных о CPU, памяти и соединениях в конфигурацию и изменил выравнивание полей структуры.

--- src/core/settings/settings.go
+++ src/core/settings/settings.go
@@ -7,12 +7,15 @@ import (
 )
 
 type Setting struct {
-	FilePath        string
-	HomePath        string
-	ScriptName      string
-	FileName        string
-	HashPass        string
-	TrafficInterval string
+	FilePath            string
+	HomePath            string
+	ScriptName          string
+	FileName            string
+	HashPass            string
+	TrafficInterval     string
+	CPUInterval         string
+	MemoryInterval      string
+	ConnectionsInterval string
 }
 
 func NewSetting() *Setting {
@@ -21,12 +24,15 @@ func NewSetting() *Setting {
 		log.Fatalf("Error loading .env file")
 	}
 	return &Setting{
-		FilePath:        os.Getenv("FILE_PATH"),
-		HomePath:        os.Getenv("HOME_PATH"),
-		ScriptName:      os.Getenv("SCRIPT_NAME"),
-		FileName:        os.Getenv("FILE_NAME"),
-		HashPass:        os.Getenv("HASH_PASS"),
-		TrafficInterval: os.Getenv("TRAFFIC_INTERVAL"),
+		FilePath:            os.Getenv("FILE_PATH"),
+		HomePath:            os.Getenv("HOME_PATH"),
+		ScriptName:          os.Getenv("SCRIPT_NAME"),
+		FileName:            os.Getenv("FILE_NAME"),
+		HashPass:            os.Getenv("HASH_PASS"),
+		TrafficInterval:     os.Getenv("TRAFFIC_INTERVAL"),
+		CPUInterval:         os.Getenv("CPU_INTERVAL"),
+		MemoryInterval:      os.Getenv("MEMORY_INTERVAL"),
+		ConnectionsInterval: os.Getenv("CONNECTIONS_INTERVAL"),
 	}
 }
 
