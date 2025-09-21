8342de06d609e8e50a8e7c1b7455a42ea6e9411a
Andrey <snick25rs@gmail.com>
2025-09-21 19:10:30 +0200

    Удалил старый файл repository.go и добавил новый.

---
+++ src/infra/monitoring/repository.go
@@ -0,0 +1,172 @@
+package monitoring
+
+import (
+	"bufio"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+	"vpn/src/core/settings"
+	"vpn/src/domain/monitoring"
+
+	"github.com/shirou/gopsutil/v3/net"
+)
+
+type Repository struct {
+	data monitoring.Monitoring
+	mu   sync.RWMutex
+}
+
+func NewRepository() *Repository {
+	r := &Repository{}
+	go r.startWriterTraffic()
+	go r.startWriterCPUPercent()
+	go r.startWriterMemoryPercent()
+	go r.startWriterGetActiveConnections()
+	return r
+}
+
+func (r *Repository) searchTun(nameTun string) (net.IOCountersStat, error) {
+	counters, err := net.IOCounters(true)
+	if err != nil {
+		log.Fatalf("Error getting IO counters: %v", err)
+	}
+	for _, c := range counters {
+		if c.Name == nameTun {
+			return c, nil
+		}
+	}
+	return net.IOCountersStat{}, nil
+}
+
+func (r *Repository) startWriterTraffic() {
+	var prevRx, prevTx uint64
+
+	trafficIntervalStr := settings.Config.TrafficInterval
+
+	trafficInterval, err := strconv.Atoi(trafficIntervalStr)
+	if err != nil {
+		log.Fatalf("Error converting traffic interval: %v", err)
+	}
+
+	for {
+		c, err := r.searchTun("tun0")
+		if err != nil {
+			log.Fatalf("Error getting IO counters: %v", err)
+		}
+
+		rxDiff := c.BytesRecv - prevRx
+		txDiff := c.BytesSent - prevTx
+
+		prevRx = c.BytesRecv
+		prevTx = c.BytesSent
+
+		down := float64(rxDiff*8) / 1_000_000 // байты -> биты -> мегабиты
+		up := float64(txDiff*8) / 1_000_000
+
+		r.mu.Lock()
+		r.data.MbpsDown = down
+		r.data.MbpsUp = up
+		r.mu.Unlock()
+		time.Sleep(time.Duration(trafficInterval) * time.Second)
+	}
+}
+
+func (r *Repository) GetTrafficUsage() (down, up float64, err error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return r.data.MbpsDown, r.data.MbpsUp, nil
+}
+
+func (r *Repository) startWriterCPUPercent() {
+	intervalStr := settings.Config.CPUInterval
+	interval, err := strconv.Atoi(intervalStr)
+	if err != nil {
+		log.Fatalf("Error converting connections interval: %v", err)
+	}
+
+	for {
+		percent, _ := cpu.Percent(time.Duration(interval)*time.Second, false) // среднее за 1 секунду
+		r.mu.Lock()
+		r.data.CPUPercentUsage = percent[0]
+		r.mu.Unlock()
+	}
+}
+
+func (r *Repository) GetCPUPercentUsage() (float64, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return r.data.CPUPercentUsage, nil
+}
+
+func (r *Repository) startWriterMemoryPercent() {
+	intervalStr := settings.Config.MemoryInterval
+
+	interval, err := strconv.Atoi(intervalStr)
+
+	v, err := mem.VirtualMemory()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		r.mu.Lock()
+		r.data.MemoryPercentUsage = v.UsedPercent
+		r.mu.Unlock()
+		time.Sleep(time.Duration(interval) * time.Second)
+	}
+}
+
+func (r *Repository) GetMemoryPercentUsage() (float64, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return r.data.MemoryPercentUsage, nil
+}
+
+func (r *Repository) readFile(path string) (int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	count := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "CLIENT_LIST") {
+			count++
+		}
+	}
+	return count, nil
+}
+
+func (r *Repository) startWriterGetActiveConnections() {
+	intervalStr := settings.Config.ConnectionsInterval
+
+	interval, err := strconv.Atoi(intervalStr)
+
+	if err != nil {
+		log.Fatalf("Error converting connections interval: %v", err)
+	}
+
+	for {
+		count, err := r.readFile("/run/openvpn-server/status-server.log")
+		if err != nil {
+			log.Fatalf("Error reading file: %v", err)
+		}
+		r.mu.Lock()
+		r.data.ActiveConnections = count
+		r.mu.Unlock()
+		time.Sleep(time.Duration(interval) * time.Second)
+	}
+}
+
+func (r *Repository) GetActiveConnections() (int, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return r.data.ActiveConnections, nil
+}
