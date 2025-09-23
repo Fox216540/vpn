f7abd9c553966b4a02c2a791efc6d9c822f972b2
Andrey <snick25rs@gmail.com>
2025-09-23 19:54:32 +0200

    Удалил возвращаемые ошибки из методов интерфейса usecase.

--- src/app/monitoring/usecase.go
+++ src/app/monitoring/usecase.go
@@ -1,9 +1,9 @@
 package monitoring
 
 type UseCase interface {
-	GetTotalMbps() (float64, error)
-	GetSplitMbps() (down, up float64, err error)
-	GetConnections() (int, error)
-	GetCPUPercent() (float64, error)
-	GetMemoryPercent() (float64, error)
+	GetTotalMbps() float64
+	GetSplitMbps() (down, up float64)
+	GetConnections() int
+	GetCPUPercent() float64
+	GetMemoryPercent() float64
 }
