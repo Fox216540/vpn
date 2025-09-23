bc1363c23826cc66177449c637526c6bab35ccc6
Andrey <snick25rs@gmail.com>
2025-09-23 19:55:02 +0200

    Добавил метод `DeleteConfigs` в интерфейс `usecase`.

--- src/app/config/usecase.go
+++ src/app/config/usecase.go
@@ -5,5 +5,6 @@ import "github.com/google/uuid"
 type UseCase interface {
 	CreateConfig(configID uuid.UUID) ([]byte, error)
 	DeleteConfig(configID uuid.UUID) error
+	DeleteConfigs(configIDs []uuid.UUID) error
 	StartServerConfig() error
 }
