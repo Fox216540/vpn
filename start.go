a678611fb49852164fe515a845cf65c362a59487
Andrey <snick25rs@gmail.com>
2025-09-24 22:49:56 +0200

    Реализовал логирование в отдельную директорию, добавил TLS для gRPC сервера и применил авторизационный интерцептор.

--- start.go
+++ start.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 	"os"
@@ -20,7 +21,11 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err := os.MkdirAll("logs", 0755); err != nil {
+		log.Fatalf("Failed to create log dir: %v", err)
+	}
+
+	file, err := os.OpenFile("logs/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("Failed to open log file: %v", err)
 	}
@@ -31,8 +36,17 @@ func main() {
 
 	configHandler := config.NewHandler()
 	h := hasher.NewHasher()
+	certFile := os.Getenv("TLS_CERT_FILE")
+	keyFile := os.Getenv("TLS_KEY_FILE")
+	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	if err != nil {
+		log.Fatalf("failed to load TLS keys: %v", err)
+	}
+
+	// Создаём gRPC сервер с TLS и интерцептором
 	server := grpc.NewServer(
-		grpc.UnaryInterceptor(interceptor.AuthUnaryInterceptor(h)),
+		grpc.Creds(creds), // 🔒 TLS
+		grpc.UnaryInterceptor(interceptor.AuthUnaryInterceptor(h)), // 🔐 твой интерцептор
 	)
 	monitoringHandler := monitoring.NewHandler()
 	pbConfig.RegisterConfigServiceServer(server, configHandler)
