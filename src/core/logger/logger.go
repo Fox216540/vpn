61b40863efc9cd7a54a3b8c4a70e14225859829c
Andrey <snick25rs@gmail.com>
2025-09-24 20:53:47 +0200

    Добавил базовую структуру логгера с уровнями Error, Info и Warn.

--- src/core/logger/logger.go
+++ src/core/logger/logger.go
@@ -11,6 +11,24 @@ type CustomLogger struct {
 	error *log.Logger
 }
 
+var Log *CustomLogger
+
+func InitLogger(out io.Writer) {
+	Log = NewCustomLogger(out)
+}
+
+func (l *CustomLogger) Error(s string) {
+	l.error.Println(s)
+}
+
+func (l *CustomLogger) Info(s string) {
+	l.info.Println(s)
+}
+
+func (l *CustomLogger) Warn(s string) {
+	l.warn.Println(s)
+}
+
 func NewCustomLogger(out io.Writer) *CustomLogger {
 	return &CustomLogger{
 		info:  log.New(out, "INFO: ", log.Ldate|log.Ltime),
