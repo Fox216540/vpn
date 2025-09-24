9bcee5e4548fcddf36d3479f6e990ab0f8f10a7b
Andrey <snick25rs@gmail.com>
2025-09-24 20:53:51 +0200

    Реализовал функцию `MapError`, которая преобразует пользовательские ошибки в ошибки gRPC со статусом.

--- src/core/mapError/map.go
+++ src/core/mapError/map.go
@@ -1,1 +1,29 @@
 package mapError
+
+import (
+	"errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"vpn/src/core/exception"
+	"vpn/src/core/logger"
+)
+
+func MapError(e error) error {
+	if e != nil {
+		logger.Log.Error(e.Error()) // логируем текст ошибки
+	}
+	var se *exception.ServerError
+	var be *exception.BadRequestError
+	var ua *exception.Unauthenticated
+
+	switch {
+	case errors.As(e, &se):
+		return status.Error(codes.Internal, "Internal server error")
+	case errors.As(e, &be):
+		return status.Error(codes.InvalidArgument, "Bad request")
+	case errors.As(e, &ua):
+		return status.Error(codes.Unauthenticated, "Unauthenticated")
+	default:
+		return nil
+	}
+}
